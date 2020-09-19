a46d4848479317f327198cff5ffb3269a1dafc76
igor matos ribeiro <igortecnologia2020@gmail.com>
2020-09-19 18:47:13 -0300

    Feito atividade MemeToNervoso

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MemeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nervoso;
+
+            //Inicio(Digite)//
+            Console.WriteLine("Usuário, o que você pensa sobre mim? ");
+            nervoso = Console.ReadLine();
+            Console.Write("\n");
+            //Fim(Digite)//
+
+            //Inicio(Nervoso)//
+            Console.WriteLine(" -E sabe o que eu penso de você?");
+            Console.WriteLine($"{nervoso.Remove(10)} ... NÃO, PERA.\nTo nervoso");
+            //Fim(Nervoso)//
+
+            //Inicio(Sair)//
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            //Fim(Sair)//
+        }
+    }
+}
